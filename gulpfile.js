3b2112cf93c8cb0bc36e1aa2bf78623e087d4201
Renato Gonçalves Dias <renatodiasbr@gmail.com>
2018-11-09 19:12:19 -0200

    ...

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const ts = require('gulp-typescript');
+
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', ['static'], () => {
+  return tsProject
+    .src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('static', ['clean'], () => {
+  return gulp.src(['src/**/*.json']).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () => {
+  return gulp.src('dist').pipe(clean());
+});
+
+gulp.task('build', ['scripts']);
+
+gulp.task('watch', ['build'], () => {
+  return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build']);
+});
+
+gulp.task('default', ['watch']);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as http from "http";
+
+import app from "./app";
+import { normalizePort, onListening, onError } from "./utils/utils";
+
+const server = http.createServer(app);
+const port = normalizePort(process.env.port || 3000);
+
+server.listen(3000);
+server.on("listening", onListening(server));
+server.on("error", onError(server));
